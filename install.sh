a3f9d38f7f8f16f011c8ef5dcc73c3d33302f36a
Ben Zatrok <ben@amberglass.co>
2023-08-09 11:25:31 +0200

    added install script

---
+++ install.sh
@@ -0,0 +1,31 @@
+
+#!/bin/bash
+
+# Variables
+APP_FOLDER="azure_uploader"
+
+# Create application folder
+cd ~
+
+if [ ! -d "$APP_FOLDER" ]
+then
+     mkdir "$APP_FOLDER"
+fi
+
+# Navigate to application folder
+cd "$APP_FOLDER"
+
+# Download the azure uploader script
+wget https://raw.githubusercontent.com/chishingchang-sparqueai/azure_uploader/main/azure_upload_linux_mac.sh
+
+# Set permissions
+chmod +x azure_upload_linux_mac.sh
+
+# Download the .env example
+wget https://raw.githubusercontent.com/chishingchang-sparqueai/azure_uploader/main/.env.example
+mv .env.example .env
+
+echo "Please edit the .env file in the $APP_FOLDER folder to configure the script for uploads."
+echo "After that, run the script with the following command:"
+echo
+echo "./azure_upload_linux_mac.sh"
