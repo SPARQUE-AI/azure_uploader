f4139f18f9de58e05d2ab92f779fb29882dab604
Ben Zatrok <ben@amberglass.co>
2023-08-09 13:51:11 +0200

    added powershell scripts

---
+++ azure_upload_windows.ps1
@@ -0,0 +1,61 @@
+$APP_FOLDER = "$HOME\azure_uploader"
+$os_name = Get-CimInstance Win32_OperatingSystem | Select-Object -ExpandProperty Caption
+
+# Install dependencies
+if (-not (Get-Command azcopy -ErrorAction SilentlyContinue)) {
+    Invoke-WebRequest -Uri "https://aka.ms/downloadazcopy-v10-windows" -OutFile AzCopy.zip -UseBasicParsing
+    Expand-Archive ./AzCopy.zip ./AzCopy -Force
+
+    # Move AzCopy
+    mkdir ~\AppData\Local\Programs\AZCopy
+    Get-ChildItem ./AzCopy/*/azcopy.exe | Move-Item -Destination ~\AppData\Local\Programs\AZCopy\
+
+    # Add AzCopy to PATH
+    $userenv = (Get-ItemProperty -Path 'HKCU:\Environment' -Name Path).path
+    $newPath = "$userenv;%USERPROFILE%\AppData\Local\Programs\AZCopy;"
+    New-ItemProperty -Path 'HKCU:\Environment' -Name Path -Value $newPath -Force
+
+    # Clean the kitchen
+    del -Force AzCopy.zip
+    del -Force -Recurse .\AzCopy\
+}
+
+# Pull in variables from .env file in the same folder as this script
+. "$APP_FOLDER\.env.ps1"
+
+# Validate variables
+if (-not $SAS_URL) {
+    Write-Output "SAS_URL is not set in the .env file. Please set it to the full SAS URL and run the script again"
+    exit
+}
+
+function UploadFile {
+    param (
+        [string]$filePath
+    )
+    
+    Write-Output "Uploading file to Azure from $filePath"
+    azcopy cp "$filePath" "$SAS_URL"
+}
+
+# Handle the base "FILE" variable if it exists
+if ($FILE_UPLOAD_PATH) {
+    UploadFile -filePath $FILE_UPLOAD_PATH
+}
+
+$index = 0
+while ($index -le 10) {
+    $var_name = "FILE_UPLOAD_PATH_$index"
+    $file = Get-Variable -Name $var_name -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Value
+
+    if (-not $file) {
+        $index++
+        continue
+    }
+
+    UploadFile -filePath $file
+
+    $index++
+}
+
+Write-Output "File upload(s) complete"
---
+++ install_windows.ps1
@@ -0,0 +1,23 @@
+# Variables
+$APP_FOLDER = "$env:USERPROFILE\azure_uploader"
+
+# Create application folder if it doesn't exist
+if (-not (Test-Path -Path $APP_FOLDER)) {
+    New-Item -Path $APP_FOLDER -ItemType Directory
+}
+
+# Navigate to application folder
+Set-Location -Path $APP_FOLDER
+
+# Download the azure uploader script
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/chishingchang-sparqueai/azure_uploader/main/azure_upload_linux_mac.sh" -OutFile "azure_upload_linux_mac.sh"
+
+# Download the .env example
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/chishingchang-sparqueai/azure_uploader/main/.env.example" -OutFile ".env.example"
+
+Write-Output "Please edit the .env file in the $APP_FOLDER folder to configure the script for uploads."
+Write-Output "We provided an example .env file (.env.example) for you to use as a starting point."
+Write-Output "Make a new .env file or rename this file to .env and edit it."
+Write-Output "After that, run the script with the following command:"
+Write-Output
+Write-Output ".\azure_upload_linux_mac.ps1"
