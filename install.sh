8382db2e6ca8daa217b7d620eafe818a6e2fa71a
Ben Zatrok <ben@amberglass.co>
2023-08-09 11:51:42 +0200

    further comments

--- install.sh
+++ install.sh
@@ -23,9 +23,10 @@ chmod +x azure_upload_linux_mac.sh
 
 # Download the .env example
 curl -sS -O https://raw.githubusercontent.com/chishingchang-sparqueai/azure_uploader/main/.env.example
-mv .env.example .env
 
 echo "Please edit the .env file in the $APP_FOLDER folder to configure the script for uploads."
+echo "We provided an example .env file (.env.example) for you to use as a starting point."
+echo "Make a new .env file or name this file to .env and edit it."
 echo "After that, run the script with the following command:"
 echo
 echo "./azure_upload_linux_mac.sh"
