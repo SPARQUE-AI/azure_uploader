fdbb2243bc8c63a60664844e7e7a0434baf0d94a
Ben Zatrok <ben@amberglass.co>
2023-08-09 11:32:30 +0200

    curl tweaks

--- install.sh
+++ install.sh
@@ -5,15 +5,13 @@
 APP_FOLDER="azure_uploader"
 
 # Create application folder
-cd ~
-
-if [ ! -d "$APP_FOLDER" ]
+if [ ! -d "~/$APP_FOLDER" ]
 then
-     mkdir "$APP_FOLDER"
+    mkdir "~/$APP_FOLDER"
 fi
 
 # Navigate to application folder
-cd "$APP_FOLDER"
+cd "~/$APP_FOLDER"
 
 # Download the azure uploader script
 curl -sS https://raw.githubusercontent.com/chishingchang-sparqueai/azure_uploader/main/azure_upload_linux_mac.sh
@@ -23,7 +21,7 @@ chmod +x azure_upload_linux_mac.sh
 
 # Download the .env example
 curl -sS https://raw.githubusercontent.com/chishingchang-sparqueai/azure_uploader/main/.env.example
-mv .env.example .env
+mv .env.example "~/$APP_FOLDER/.env"
 
 echo "Please edit the .env file in the $APP_FOLDER folder to configure the script for uploads."
 echo "After that, run the script with the following command:"
